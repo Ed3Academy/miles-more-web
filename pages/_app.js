d56ff8186a94e8f2c4df1880d4530bf827901455
tianxiuali <627786255@qq.com>
2023-06-27 11:40:35 +0800

    feat: add task1 code

---
+++ pages/_app.js
@@ -0,0 +1,32 @@
+import '../styles/globals.css'
+import '@rainbow-me/rainbowkit/styles.css'
+import { getDefaultWallets, RainbowKitProvider } from '@rainbow-me/rainbowkit'
+import { configureChains, createClient, WagmiConfig } from 'wagmi'
+import { mainnet, polygon, polygonMumbai } from 'wagmi/chains'
+import { publicProvider } from 'wagmi/providers/public'
+
+const { chains, provider, webSocketProvider } = configureChains([mainnet, polygon, polygonMumbai], [publicProvider()])
+
+const { connectors } = getDefaultWallets({
+  appName: 'RainbowKit App',
+  chains
+})
+
+const wagmiClient = createClient({
+  autoConnect: true,
+  connectors,
+  provider,
+  webSocketProvider
+})
+
+function MyApp({ Component, pageProps }) {
+  return (
+    <WagmiConfig client={wagmiClient}>
+      <RainbowKitProvider chains={chains}>
+        <Component {...pageProps} />
+      </RainbowKitProvider>
+    </WagmiConfig>
+  )
+}
+
+export default MyApp
